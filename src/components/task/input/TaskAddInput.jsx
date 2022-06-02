a88aad489de36389cf25e58818e921200e393c29
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-06-02 22:22:53 +0900

    add: TaskAddInput

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,9 +1,15 @@
 import React from 'react'
 
 export const TaskAddInput = () => {
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(e);
+    };
     return (
         <div>
-            
+            <form onSubmit={handleSubmit}>
+                <input className="taskAddInput" type="text" placeholder="add a task" />
+            </form>
         </div>
     )
 }
