70f2f4e40934a6a174265b64fdd716f3153b4bdb
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-06-02 23:27:23 +0900

    add: ~35

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Task } from "./Task"
 
-export const Tasks = () => {
+export const Tasks = ({inputText, taskList}) => {
     return (
         <div>
-            
+            {taskList.map(task => (
+                <div>
+                    <Task task={task} />
+                </div>
+            ))}
         </div>
     )
 }
--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,14 +1,25 @@
 import React from 'react'
 
-export const TaskAddInput = () => {
+export const TaskAddInput = ({inputText, setInputText, taskList, setTaskList}) => {
     const handleSubmit = (e) => {
         e.preventDefault();
         console.log(e);
+        // カードを追加する
+        setTaskList([
+            ...taskList,
+            {
+                text:inputText,
+            },
+        ]);
+        setInputText("");
+    };
+    const handleChange = (e) => {
+        setInputText(e.target.value);
     };
     return (
         <div>
             <form onSubmit={handleSubmit}>
-                <input className="taskAddInput" type="text" placeholder="add a task" />
+                <input className="taskAddInput" type="text" placeholder="add a task" onChange={handleChange} value={inputText} />
             </form>
         </div>
     )
