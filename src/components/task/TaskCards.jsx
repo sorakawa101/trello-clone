c49e0bc2fcf6237ec1585904c955420a9c76a700
sorakawa101 <s2230063@edu.cc.uec.ac.jp>
2022-06-03 12:43:25 +0900

    add: 43~44

--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -3,11 +3,21 @@ import { TaskCard } from "./TaskCard"
 import { AddTaskCardButton } from "./button/AddTaskCardButton"
 
 export const TaskCards = () => {
-    const [taskCardsList, setTaskCardsList] = useState([]);
+    const [taskCardsList, setTaskCardsList] = useState([
+        {
+            id: 0,
+            draggableId: "item0",
+        },
+    ]);
     return (
         <div className="taskCardsArea">
-            {taskCardsList.map((taskCardList) => (
-                <TaskCard key={taskCardList.id}/>
+            {taskCardsList.map((taskCard) => (
+                <TaskCard
+                    key={taskCard.id}
+                    taskCardsList={taskCardsList}
+                    setTaskCardsList={setTaskCardsList}
+                    taskCard={taskCard}
+                />
             ))}
             <AddTaskCardButton
                 taskCardsList={taskCardsList}
